e1b3ad3ba22a43dfa666aadd1c98cb49ee03d5d2
zlago <foxesarethecutestowo@gmail.com>
2022-12-17 22:06:42 +0100

    i have no idea what i have done
    controller input, include graphics, actually add another file
    and i forgot to commit that.. yikes!

--- build.bat
+++ build.bat
@@ -1,7 +1,8 @@
 @ ECHO OFF
 :: assemble each file
-ca65 -I src/inc/ src/main.65 -o obj/main.o
+ca65 -g -I src/inc/ src/main.65 -o obj/main.o
+ca65 -g -I src/inc/ src/input.65 -o obj/input.o
 :: link the object files
-ld65 -C nrom128.cfg obj/main.o -o bin/test.nes
+ld65 --dbgfile bin/test.dbg -C nrom128.cfg obj/main.o obj/input.o -m bin/test.txt -o bin/test.nes
 :: done
 pause
