db2ccd579781d18c84c4abf268692e80df12daa9
zlago <104219492+zlago@users.noreply.github.com>
2022-12-28 18:32:55 +0100

    change include and import extensions
    add a distinction between include (.i) and import (.imp) files
    change include file extension from .inc to .i
    (so sublime stops trying to use rgbds syntax,
    id be using .s if it wasnt used by some syntax package)

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @ ECHO OFF
 :: assemble each file
-ca65 -g -I src/inc/ src/main.65 -o obj/main.o
-ca65 -g -I src/inc/ src/input.65 -o obj/input.o
+ca65 -g -I src/inc/ -I src/imp/ src/main.65 -o obj/main.o
+ca65 -g -I src/inc/ -I src/imp/ src/input.65 -o obj/input.o
 :: link the object files
 ld65 --dbgfile bin/test.dbg -C nrom128.cfg obj/main.o obj/input.o -m bin/test.txt -o bin/test.nes
 :: done
