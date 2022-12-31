3ed6269720d3afee0f291dab27d6e119de0d4e84
zlago <104219492+zlago@users.noreply.github.com>
2022-12-31 14:03:00 +0100

    give NMI and IRQ their own files

--- build.bat
+++ build.bat
@@ -3,7 +3,11 @@
 ca65 -g -I src/inc/ -I src/imp/ src/main.65  -o obj/main.o
 ca65 -g -I src/inc/ -I src/imp/ src/input.65 -o obj/input.o
 ca65 -g -I src/inc/ -I src/imp/ src/reset.65 -o obj/reset.o
+ca65 -g -I src/inc/ -I src/imp/ src/irq.65   -o obj/irq.o
+ca65 -g -I src/inc/ -I src/imp/ src/nmi.65   -o obj/nmi.o
+
 :: link the object files
-ld65 --dbgfile bin/test.dbg -C nrom128.cfg obj/main.o obj/input.o obj/reset.o -m bin/test.txt -o bin/test.nes
+ld65 --dbgfile bin/test.dbg -C nrom128.cfg obj/main.o obj/input.o obj/reset.o -m bin/test.txt obj/irq.o obj/nmi.o -o bin/test.nes
+
 :: done
 pause
